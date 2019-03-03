6d345026431682355620fd4d6fdaebd01e0804ae
raboag927 <rexford_aboagye@comcast.com>
2019-03-02 21:22:34 -0500

    Added tests

---
+++ src/test/CreditCardTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import static org.junit.Assert.*;
+
+public class CreditCardTest {
+
+    @org.junit.Test
+    public void isValid() {
+    }
+
+    @org.junit.Test
+    public void sumOfDoubleEvenPlace() {
+    }
+
+    @org.junit.Test
+    public void getDigit() {
+    }
+
+    @org.junit.Test
+    public void sumOfOddPlace() {
+    }
+
+    @org.junit.Test
+    public void prefixMatched() {
+    }
+
+    @org.junit.Test
+    public void getSize() {
+    }
+
+    @org.junit.Test
+    public void getPrefix() {
+    }
+
+    @org.junit.Test
+    public void inputData() {
+    }
+
+    @org.junit.Test
+    public void displayResult() {
+    }
+}
